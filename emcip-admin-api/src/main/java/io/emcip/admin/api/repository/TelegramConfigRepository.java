8335d9fa90701b60d49c672bd130fc9a843c44be
Benjamin Marstaller <theyellow@gmx.de>
2026-04-26 21:33:13 +0200

    chore(admin-api): remove TelegramConfig, TelegramController, TelegramConfigRepository

--- emcip-admin-api/src/main/java/io/emcip/admin/api/repository/TelegramConfigRepository.java
+++
@@ -1,6 +0,0 @@
-package io.emcip.admin.api.repository;
-
-import io.emcip.admin.api.entity.TelegramConfig;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-public interface TelegramConfigRepository extends ReactiveCrudRepository<TelegramConfig, Long> {}
