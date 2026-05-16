9e482467d6cb903ebc86b4a4410414b8e8418834
Benjamin Marstaller <theyellow@gmx.de>
2026-05-16 22:16:48 +0200

    feat(openapi): add @SecurityScheme to all OpenApiConfig classes
    admin-api: bearerAuth (HTTP Bearer JWT, obtained from POST /api/auth/token)
    All other services: serviceToken (X-Service-Token header, service-to-service auth)
    Global @SecurityRequirement applied via @OpenAPIDefinition so all endpoints
    show the lock icon in Swagger UI without per-endpoint annotations.
    
    Closes backlog item 4b.

--- emcip-moderation-service/src/main/java/io/emcip/moderation/service/config/OpenApiConfig.java
+++ emcip-moderation-service/src/main/java/io/emcip/moderation/service/config/OpenApiConfig.java
@@ -1,7 +1,11 @@
 package io.emcip.moderation.service.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(
@@ -9,6 +13,13 @@ import org.springframework.context.annotation.Configuration;
                 @Info(
                         title = "Moderation Service API",
                         description = "Moderation rule management and toxicity filtering",
-                        version = "1.0"))
+                        version = "1.0"),
+        security = @SecurityRequirement(name = "serviceToken"))
+@SecurityScheme(
+        name = "serviceToken",
+        type = SecuritySchemeType.APIKEY,
+        in = SecuritySchemeIn.HEADER,
+        paramName = "X-Service-Token",
+        description = "Internal service token for service-to-service authentication")
 @Configuration
 public class OpenApiConfig {}
